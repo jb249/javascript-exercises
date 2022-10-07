038b4420b8ec5a1a1168e1728822488bd41d42a9
Jack Butler <jack.butler19@protonmail.com>
2022-10-07 18:22:36 -0400

    Declare repeatString function.
    Function takes string and number as input. Running string is declared
    in function as an empty string, and the input string is concatenated
    with the running string as many times as the input number. Passed
    first test.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+function repeatString(string, num) {
+    runningString = "";
+    for (let i = 0; i < num; i++) {
+        runningString = runningString.concat(string);
+    }
+    return runningString;
 };
 
 // Do not edit below this line
