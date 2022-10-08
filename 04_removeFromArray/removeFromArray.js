468037a9694b9a303a4854032040fe41a4ec7fdf
Jack Butler <jack.butler19@protonmail.com>
2022-10-08 15:35:42 -0400

    Declare removeFromArray function.
    Function finds index of element that needs to be removed. It then splices
    the array and removes the element at that index. Can only accept one
    element.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, item1) {
+    index = array.indexOf(item1);
+    if (index > 0) {
+        array.splice(index, 1);
+    }
+    return array;
 };
 
 // Do not edit below this line
