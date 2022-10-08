3e3f6736a4f63e95752d51cbcc7586950a25c890
Jack Butler <jack.butler19@protonmail.com>
2022-10-08 17:24:28 -0400

    Redo function with rest parameter.
    Added rest parameter, which the function then iterates through to remove
    elements from the rest parameter array that are in the input array. Does
    not yet test the type of the element.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,10 +1,19 @@
-const removeFromArray = function(array, item1) {
-    index = array.indexOf(item1);
-    if (index > 0) {
-        array.splice(index, 1);
+const removeFromArray = function(array, ...moreArgs) {
+    //console.log(array);
+    //console.log(moreArgs);
+    for (const arg of moreArgs) {
+        index = array.indexOf(arg);
+        if (index > -1) {
+            array.splice(index, 1);
+        }
     }
+    //console.log(array);
     return array;
 };
+
+//const array1 = [1,2,3,4];
+
+//removeFromArray(array1, 1,2,3,4);
 
 // Do not edit below this line
 module.exports = removeFromArray;
