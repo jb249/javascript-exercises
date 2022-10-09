00b5181403d66a2692bd6401327e6c98ad55058c
Jack Butler <jack.butler19@protonmail.com>
2022-10-08 20:01:39 -0400

    Declare sumAll function to pass initial 2 tests.
    Function takes 2 numbers as input and uses an internal sum and
    increase integer to iterate through each number in between the 2
    input numbers. Passed first 2 tests. Can detect errors if input is
    negative or not a number.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let sum = num1;
+    let increase = num1 + 1;
+    if (num1 < 0 || num2 < 0) {
+        return "ERROR!";
+    }
+    if (typeof num1 != 'number' || typeof num2 != 'number') {
+        return "ERROR!";
+    }
+    for (let i = 0; i < (num2 - num1); i++) {
+        sum += increase;
+        increase += 1;
+    }
+    return sum;
 };
 
 // Do not edit below this line
