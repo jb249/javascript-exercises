332383e9d4face17c86f4e2ab4ce6bd9b1fbedc5
Jack Butler <jack.butler19@protonmail.com>
2022-10-08 20:32:38 -0400

    Declare function with given conditionals. Passed first test.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,16 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    //console.log(year % 400 == 0);
+    //console.log(year % 4 == 0 && year % 100 != 0);
+    if (year % 400 == 0) {
+        return true;
+    } else if (year % 4 == 0 && year % 100 != 0) {
+        return true;
+    } else {
+        return false;
+    }
 };
+//leapYears(1996)
+//leapYears(2000)
 
 // Do not edit below this line
 module.exports = leapYears;
