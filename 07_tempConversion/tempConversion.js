d30e0e57baab4330d56ee3a23256a95bc19b232f
Jack Butler <jack.butler19@protonmail.com>
2022-10-10 09:11:43 -0400

    Declare initial functions to convert from each.
    Used formula to convert from F to V and vice versa. Passed
    test for converting freezing from one to the other. Does not
    validate input or round to the tenths place.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(tempFahrenheit) {
+  convertedTemp = (tempFahrenheit - 32) * (5/9);
+  return convertedTemp;
 };
 
-const ctof = function() {
-
+const ctof = function(tempCelsius) {
+  convertedTemp = tempCelsius * (9/5) + 32;
+  return convertedTemp;
 };
 
 // Do not edit below this line
