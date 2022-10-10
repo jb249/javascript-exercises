4f31565fbdf7a177071f67ab36368c16b1f65309
Jack Butler <jack.butler19@protonmail.com>
2022-10-10 09:20:30 -0400

    Declare round function to round to tenths place.
    Created a rounding function that rounds the input value to the input
    precision. Used this function in both ftoc and ctof to round to
    tenths place. Works with negative numbers as well. Passed all tests.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,16 @@
+function round(value, precision) {
+  const multiplier = Math.pow(10, precision || 0);
+  return Math.round(value * multiplier) / multiplier;
+}
+
 const ftoc = function(tempFahrenheit) {
   convertedTemp = (tempFahrenheit - 32) * (5/9);
-  return convertedTemp;
+  return round(convertedTemp, 1);
 };
 
 const ctof = function(tempCelsius) {
   convertedTemp = tempCelsius * (9/5) + 32;
-  return convertedTemp;
+  return round(convertedTemp, 1);
 };
 
 // Do not edit below this line
